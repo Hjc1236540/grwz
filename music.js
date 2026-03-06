e2d4d4d43abe155e934e7cf03f0d05bf002a1e4b
Hjc1236540 <chaojunhu521@163.com>
2026-03-06 19:09:36 +0800

    Update music.js

--- music.js
+++ music.js
@@ -11,7 +11,7 @@
     { title: '月亮照山川', src: 'http://fsios.kugou.com/202602281107/cd46e7a03b953ede36bb8288c2877ea0/v3/9bbb2b020be26dd4c0e65d3d502bf115/yp/full/pi3_ct310000_s2840147546.mp3' },
     { title: '桃花笑', src: 'https://aqqmusic.tc.qq.com/M500000nsRbX40jRMw.mp3?guid=123759963&vkey=7D3A7979200FD843EACA9533590CCF73D43B253B39179219C68D7AA67E7EB0E330DF6BFDF28FBDEC3EBFEEB9179DD860BB3CA27162A15B0F__v2b9abf46&uin=&fromtag=120042&src=M500001OwoYa1ebPSv.mp3' },
     { title: '像我这样的人', src: 'https://aqqmusic.tc.qq.com/M500002wRxXe0eLiiT.mp3?guid=242233267&vkey=7D8DCFEA067AA2A86B9FA1020519E9FB83A7DA7CB7E70FDFF8B0BA1FBBE5BCFA3DA93CA32A15B024C3FE5593A84ED796817057E0EFB8F5D1__v2b9abf42&uin=&fromtag=120042&src=M500003mU3v20Bny9T.mp3' },
-    { title: '月亮照山川', src: 'http://fsios.kugou.com/202602281107/cd46e7a03b953ede36bb8288c2877ea0/v3/9bbb2b020be26dd4c0e65d3d502bf115/yp/full/pi3_ct310000_s2840147546.mp3' },
+    { title: '今晚不想睡', src: './music/music14wdm.mp3' },
     { title: '良人归', src: 'https://aqqmusic.tc.qq.com/M500003JGHsA3Pae0S.mp3?guid=1819623842&vkey=C1969810CE19C9B284FA80A2BA83FE8723FF2CC0279E15EAD468E551E3EE323900020F629C924F5846B8E46A5FA831FC3713F77E296DAECB__v2b9abf46&uin=&fromtag=120042&src=M5000014rSRG0gfzNa.mp3' },
     { title: '卡尔永远的神', src: 'https://aqqmusic.tc.qq.com/M500004Ywgu71P659A.mp3?guid=367313220&vkey=35F5E39C3B49B9719D95618D03BE6BBDD8884881666E780E9D7909B3E88BA99F2BFEE642992C945CA4771DEA0E3F8146461A3657F670C7D2__v2b9abf46&uin=&fromtag=120042&src=M500002Vhay20Dd3Vm.mp3' },
     { title: '半点心', src: 'https://aqqmusic.tc.qq.com/M500001iwxIP4O1cDs.mp3?guid=650799268&vkey=76CF389BA9731E53F64FB37F85EAB3428F719D7FA75EF0879CB15425D8B03331774992DDBCDE5036DFD411811DBDB0B8D405F6CE42F6464F__v2b9abf46&uin=&fromtag=120042&src=M500000re83G3zeP1I.mp3' },
@@ -20,7 +20,13 @@
     { title: '怎么说我不爱你', src: 'https://aqqmusic.tc.qq.com/M500004VSVzG0We1cD.mp3?guid=1761020647&vkey=9E93E9B0BB3D7314FB24DE7D5F4977FEB0534A1D61C4FED8EEF9CCBE8B895C84CA9369E4C7AF8697765BDA4A20D9277B5773043592A6CEE3__v2b9abf42&uin=&fromtag=120042&src=M500000sjT0f1UnEGz.mp3' },
     { title: '你说没烦恼', src: 'https://aqqmusic.tc.qq.com/M50000195PNt0fzG3e.mp3?guid=2141980762&vkey=FB0FA7F5695834607085362FF4C33785866A699AB8AA9B64DE9FC1BA88C391CE341C48595C021B23B11E5E80291A0988CF64A8045368F6AC__v2b9aaeb9&uin=&fromtag=120042&src=M500000MJ4ds3qzZWX.mp3' },
     { title: '人生走一遭', src: 'https://aqqmusic.tc.qq.com/M500002BbMcd0731mr.mp3?guid=389724115&vkey=0D9059A01B8DFAA7F76A18713B23F27C6423E521A3D5E86813141CA0DEF19D1F627E18B4E1DA01B262CA5405D9EED295EC6675A4D0443C04__v2b9ab03c&uin=&fromtag=120042&src=M500004KRxbc2GR0Ag.mp3' },
-    { title: '别怕我伤心 广西神曲DJ', src: 'https://aqqmusic.tc.qq.com/M500004CNNsf09cfVC.mp3?guid=1568271838&vkey=E099FFC04D9BCAD8CEACE12F759B9942CFC80ED83B20733C8D1E3CAD3B721472F8740601149967B869F7D557C94504FDF084B96ACE39730B__v2b9aaeb9&uin=&fromtag=120042&src=M500003xEmCl2qH2gM.mp3' }
+    { title: '别怕我伤心 广西神曲DJ', src: 'https://aqqmusic.tc.qq.com/M500004CNNsf09cfVC.mp3?guid=1568271838&vkey=E099FFC04D9BCAD8CEACE12F759B9942CFC80ED83B20733C8D1E3CAD3B721472F8740601149967B869F7D557C94504FDF084B96ACE39730B__v2b9aaeb9&uin=&fromtag=120042&src=M500003xEmCl2qH2gM.mp3' },
+    { title: '自律才是修行', src: './music/music15zilv.mp3' },
+    { title: '马上发大财', src: './music/music16msfc.mp3' },
+    { title: '夜萤火虫', src: './music/music17yhc.mp3' },
+    { title: '风之谷', src: './music/music18fzg.mp3' },
+    { title: '一点点', src: './music/music19ydd.mp3' },
+    { title: '多幸运', src: './music/music20dxy.mp3' }
 ];
 
 let currentSongIndex = 0;
